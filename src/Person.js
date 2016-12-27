73a40746c63682c8552a248731a943e9067509cd
Abdoul Sy <dreescan@gmail.com>
2016-12-27 14:31:31 +0000

    visual enhancements

--- src/Person.js
+++ src/Person.js
@@ -5,28 +5,38 @@ import './Person.css';
 class Person extends Component {
   render() {
     return (
-      <div className="person" itemScope itemType="http://schema.org/Person">
-        <h1 itemProp="name">Abdoul Sy</h1>
-        <h3 itemProp="jobTitle">Developer</h3>
-        <h5 itemProp="email">message@abdoulsy.eu</h5>
-        <div itemScope itemType="http://schema.org/PostalAddress">          <p itemProp="addressLocality">London</p>
-          <p itemProp="addressRegion">Camden</p>
-          <p itemProp="postalCode">NW61DP</p>
-          <p itemProp="streetAddress">7 Burgess Park Mansions,
-          <br/> Fortune Green Road. </p>
-        </div>
+      <div className="Person" itemScope itemType="http://schema.org/Person">
+        <header className="Person-header">
+          <div className="Person-basic">
+            <h1 itemProp="name">Abdoul Sy</h1>
+            <h3 itemProp="jobTitle">Developer</h3>
+            <h5 itemProp="email">message@abdoulsy.eu</h5>
+          </div>
+          <div className="Person-avatar">
+            <div itemProp="image" className="PersonImage">
+              <img src={avatar} role="presentation"
+                alt="Abdoul Sy"/>
+            </div>
+          </div>
+        </header>
         <p itemProp="telephone">(+44) 07 810 763 236</p>
-        <p itemProp="url">http://web.abdoulsy.eu</p>
+        <p itemProp="url">
+          <a href="http://web.abdoulsy.eu">
+           http://web.abdoulsy.eu
+          </a>
+        </p>
         <div itemProp="colleague">
           <p>Colleague </p>
           <div className="colleagueList">
-            <p itemScope itemType="Person">Ozzy</p>
+            <p itemScope itemType="Person">Ousmane Ndiaye</p>
           </div>
         </div>
-        <div itemProp="image" className="PersonImage">
-          <img src={avatar} role="presentation"
-            alt="Abdoul Sy"/>
-
+        <div className="Person-postalAddress" itemScope itemType="http://schema.org/PostalAddress">
+          <p itemProp="streetAddress">7 Burgess Park Mansions,
+          <br/> Fortune Green Road. </p>
+          <span itemProp="addressRegion">Camden</span>,
+          <span itemProp="postalCode">NW61DP</span>,
+          <span itemProp="addressLocality">London</span>
         </div>
       </div>
     );
